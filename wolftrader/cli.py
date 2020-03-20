7c1044ba48976c24e7ef39b730ac9f173183c391
Nicolas Nunez <nicolasnunezromay@gmail.com>
2020-03-20 12:43:16 -0500

    Refactoring of CLI moved sys.args -> argparse

--- wolftrader/cli.py
+++ wolftrader/cli.py
@@ -8,41 +8,59 @@
 """
 import sys
 import time
+import argparse
 from wolf import mine, process, notify, trade
-#
-if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        if sys.argv[1] == 'data_mine':
-            while True:
-                mine()
-                time.sleep(600)
-        elif sys.argv[1] == 'data_process':
-            while True:
-                process()
-                time.sleep(600)
-        elif sys.argv[1] == 'notify':
-            while True:
-                notify()
-                time.sleep(3600*4)
-        elif sys.argv[1] == 'trade':
-            while True:
-                trade()
-                time.sleep(3600*8)
-        else:
-            print('Parameter not recognized')
+from util.logger import log_info
+
+
+parser = argparse.ArgumentParser(description='CLI to mine, trade, calculate BitCoin',
+                                 usage='python cli.py action --frequency=10 (Run action every 10 minutes)\n'
+                                       'python cli.py data-mine --frequency=30 (Get prices every 30 minutes)',
+                                 add_help=False
+                                 )
+parser.add_argument('action',
+                    type=str,
+                    choices=['data-mine', 'data-process', 'notify', 'trade'],
+                    help='Select which to run...')
+parser.add_argument('-f',
+                    '--frequency',
+                    type=int,
+                    # choices=list(range(15, 1441, 15)),
+                    default=10,
+                    help='Frequency for action, (run action after every n minutes)')
+parser.add_argument('-h', '--help',
+                    action='help',
+                    help='-f --frequency in minutes, specifies how often to execute\n'
+                         'action what you want to run')
+args = parser.parse_args()
+
+log_info(f'Started Wolftrader from {__file__}, '
+         f'with parameter: {args.action} '
+         f'and frequency:  {args.frequency}')
+
+time_in_seconds = args.frequency * 60
+try:
+    if args.action == 'data-mine':
+        while True:
+            mine()
+            time.sleep(time_in_seconds)
+    elif args.action == 'data-process':
+        while True:
+            process()
+            time.sleep(time_in_seconds)
+    elif args.action == 'notify':
+        while True:
+            notify()
+            time.sleep(time_in_seconds)
+    elif args.action == 'trade':
+        while True:
+            trade()
+            time.sleep(time_in_seconds)
     else:
-        print ("The fuck you talkin 'bout playa")
-
-
-def miner():
-    mine()
-
-def notifier():
-    notify()
-
-def processor():
-    process()
-
-def trader():
-    trade()
-
+        print('Parameter not recognized')
+except KeyboardInterrupt as e:
+    pass
+finally:
+    log_info(f'Ended Wolftrader from {__file__}, '
+             f'with parameter: {args.action} '
+             f'and frequency:  {args.frequency}')
